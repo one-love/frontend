79d6d5ba7b37a8bdf909f7e798b4336bcd042e87
Mladen Stepanic <mladen.stepanic@sixsentix.com>
2014-09-28 18:46:23 +0200

    Adds browserify support, fixes: #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var paths = {
   sass: 'app/sass/**/*.scss',
   css: 'app/css/**/*.css',
-  cssFolder: 'app/css'
+  cssFolder: 'app/css',
+  jsEntry: 'app/scripts/index.js',
+  scripts: 'app/scripts/**/*.js'
 };
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-ruby-sass');
+var browserify = require('gulp-browserify');
 
 /**
  * Core development server task.
  * Initializes connect webserver as gulp task, with livereload and SSL enabled.
  */
-gulp.task('webserver', ['compile:sass', 'watch'], function() {
+var webserverDeps = ['compile:sass', 'compile:browserify', 'watch'];
+gulp.task('webserver', webserverDeps, function() {
   return gulp.src('app')
     .pipe(webserver({
       port: 8000,
@@ -37,8 +41,23 @@ gulp.task('compile:sass', function() {
     .pipe(gulp.dest(paths.cssFolder));
 });
 
+/**
+ * Browserify allows use of node.js tools in browser environment.
+ */
+gulp.task('compile:browserify', function() {
+  gulp.src(paths.jsEntry)
+    .pipe(browserify({
+      debug: true
+    }))
+    .on('error', function(error) {
+      console.log(error);
+    })
+    .pipe(gulp.dest('app/js'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['compile:sass'] )
+  gulp.watch(paths.scripts, ['compile:browserify'] )
 });
 
 gulp.task('default', ['webserver']);
