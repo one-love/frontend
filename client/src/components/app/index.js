763482a291169c8e7109ee5d1aed5fdbdbe8c82a
Mladen Stepanic <crawleyprint@gmail.com>
2016-01-16 11:40:56 +0100

    Add react router with sample routes

---
+++ client/src/components/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OneLove = React.createClass({
+  render() {
+    return (
+      <main className="app">
+        <a href="/#/login">Go to Login</a>
+      </main>
+    );
+  },
+});
+
+export default OneLove;
--- client/src/entry.js
+++ client/src/entry.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import LoginForm from './components/hello';
+import OneLove from './components/app';
+import {
+  Route,
+  Router,
+} from 'react-router';
 
-ReactDom.render(
-  <LoginForm />,
-  document.getElementById('onelove')
-);
+ReactDom.render((
+  <Router>
+    <Route path="/" component={OneLove} />
+    <Route path="/login" component={LoginForm} />
+  </Router>
+), document.getElementById('onelove'));
