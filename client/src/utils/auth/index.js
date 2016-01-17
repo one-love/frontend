52ac69e8992075d5888c70e294802e6e3c355d69
Goran Mekić <meka@lugons.org>
2016-01-17 10:13:09 +0100

    Hardcoded version of superagent usage

--- client/src/utils/auth/index.js
+++ client/src/utils/auth/index.js
@@ -1,3 +1,7 @@
+import superagent from 'superagent-bluebird-promise';
+import superagentJsonapify from 'superagent-jsonapify';
+
+superagentJsonapify(superagent);
 /**
  * Return authentication token for One Love
  *
@@ -14,4 +18,23 @@ export function getAuthToken() {
  */
 export function isLoggedIn() {
   return Boolean(getAuthToken());
+}
+
+/**
+ * Get token from backend API and store it in local storage
+ */
+export function login(argemail, argpassword) {
+  superagent.post('http://localhost:5000/api/v0/auth/tokens')
+    .send({ email: argemail, password: argpassword })
+    .then(
+      function success(res) {
+        window.localStorage.OneLoveAuthToken = res.body.token;
+        return res.body.token;
+      }
+    )
+    .catch(
+      function error(err) {
+        return err;
+      }
+    );
 }
