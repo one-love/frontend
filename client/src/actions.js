e57dc5728a3a60c619d2c1e188f72d994a653ffc
Goran Mekić <meka@lugons.org>
2016-01-21 10:30:42 +0100

    Remove dumb comment

--- client/src/actions.js
+++ client/src/actions.js
@@ -14,12 +14,6 @@ export const saveToken = createAction(LOGIN, json => {
   return json;
 });
 
-// This is a thunk, meaning it is a function that immediately
-// returns a function for lazy evaluation. It is incredibly useful for
-// creating async actions, especially when combined with redux-thunk!
-// NOTE: This is solely for demonstration purposes. In a real application,
-// you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
-// reducer take care of this logic.
 export const login = (email, password) => {
   return dispatch => {
     fetch(
