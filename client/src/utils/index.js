bd3b61bac232cad9f0391e6b31276502a89c3d26
Mladen Stepanic <crawleyprint@gmail.com>
2016-01-21 20:31:27 +0100

    Add docblock to utils

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import isomorphicFetch from 'isomorphic-fetch';
 
 
-export function wrapComponent(Component, props) {
+/**
+ * Pass store as props
+ *
+ * @param component - component that will receive props
+ * @param props - props to pass to component as state
+ */
+export function wrapComponent(component, props) {
   return React.createClass({
     propTypes: {
       children: React.PropTypes.node,
     },
 
     render() {
-      return React.createElement(Component, props, this.props.children);
+      return React.createElement(component, props, this.props.children);
     },
   });
 }
