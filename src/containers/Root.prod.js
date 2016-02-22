a718e3bc7f867c25775fe0e350b8356bd62d0dca
Goran Mekić <meka@lugons.org>
2016-02-22 12:33:11 +0100

    Cleanup

--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
-import OneLoveApp from './OneLove';
+import React, { PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import Main from './Main';
 
-export default class Root extends Component {
+
+const Root = React.createClass({
+  propTypes: {
+    store: PropTypes.object,
+  },
+
   render() {
+    const { store } = this.props;
     return (
-      <OneLoveApp />
+      <Provider store={store}>
+        <div>
+          <Main />
+        </div>
+      </Provider>
     );
-  }
-}
+  },
+});
+
+export default Root;
