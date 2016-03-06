57e01ed44a7fee5637e14601c13a4230fa148d76
Veljko Simic <veljko.simic98@gmail.com>
2016-03-06 18:55:24 +0100

    Testing success state in cluster component

--- src/components/cluster/tests/create.js
+++ src/components/cluster/tests/create.js
@@ -14,6 +14,16 @@ const clusterCreateTest = describe('Testing create of cluster', () => {
         type: CLUSTER_CREATE,
      })
   }),
+   it('get success state', () => {
+     expect(store.dispatch(actions.success({})))
+      .toEqual({
+        payload: {
+          cluster: {},
+          status: 'success',
+        },
+        type: CLUSTER_CREATE,
+     })
+  }),
    it('get pending state', () => {
      expect(store.dispatch(actions.begin()))
       .toEqual({
