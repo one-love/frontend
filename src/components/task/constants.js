e399f3920f8ffbe52a0f43b0cd643313b4e9edd6
Veljko Simic <veljko.simic98@gmail.com>
2016-04-16 15:03:18 +0200

    Create list of task

--- src/components/task/constants.js
+++ src/components/task/constants.js
@@ -1,1 +1,2 @@
 export const TASK_DETAIL = 'TASK_DETAIL';
+export const TASK_LIST = 'TASK_LIST';
--- src/components/task/index.js
+++ src/components/task/index.js
@@ -1,25 +1,70 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import actions from './actions/list';
+import store from '../../store';
 import detail from './components/detail';
 
+const mapStateToProps = state => ({
+  tasks: state.taskList.tasks,
+  status: state.taskList.status,
+});
+
 const Component = React.createClass({
+  propTypes: {
+    children: React.PropTypes.node,
+    tasks: React.PropTypes.array,
+    status: React.PropTypes.string,
+  },
+
+  componentWillMount() {
+    store.dispatch(actions.get());
+  },
+
+  componentWillUnmount() {
+    store.dispatch(actions.reset());
+  },
+
   render() {
-    const index = (
+    console.log(this.props);
+    const children = (
       <div>
-        Hello from tasks
+        {this.props.children}
       </div>
     );
+    if (this.props.tasks === undefined) {
+      return <div></div>;
+    }
+    const index = (
+      <div>
+        <h2>My Task:</h2>
+        <ul>
+          {
+            this.props.tasks.map(
+              task =>
+              <li key={task.id}>
+              <Link
+                key={task.id}
+                to={`/tasks/${task.id}/`}
+              > {task.id} </Link> </li>
+            )
+          }
+        </ul>
+      </div>
+    );
+    if (this.props.children) {return children;}
     return index;
   },
 });
 
-export const List = Component;
+export const List = connect(mapStateToProps, actions)(Component);
 
-const router = {
+const routes = {
+  path: 'tasks',
   indexRoute: { component: List },
-  path: '/tasks/',
   childRoutes: [
     detail,
   ],
 };
 
-export default router;
+export default routes;
