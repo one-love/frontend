e8e5c9c996ebd802c79e0266f2e077baa570fb4d
Goran Mekić <meka@tilda.center>
2016-04-30 11:10:44 +0200

    Send token to backend on connect

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,6 @@
 import { hashHistory } from 'react-router';
-import io from 'socket.io-client';
-import { SOCKETIO_URL } from './backend_url';
 
 export const history = hashHistory;
 export const postLogoutURL = '/login/';
 
-export const socketio = io(SOCKETIO_URL, { transports: ['websocket'] });
+export const socket = {};
