4c69ae056750964ebe31f5a296777da91e4230fb
Goran Mekić <meka@tilda.center>
2016-05-05 19:42:03 +0200

    Docker compose

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 export PROJECT_ROOT=$(readlink -f "$(dirname $0)/..")
+export NODE_ENV="dev"
+cp "${PROJECT_ROOT}/src/backend_url.dev.js" "${PROJECT_ROOT}/src/backend_url.js"
 cd ${PROJECT_ROOT}
+npm install
 npm start
---
+++ src/backend_url.dev.js
@@ -0,0 +1,2 @@
+export const SOCKETIO_URL = 'http://localhost:5000/onelove';
+export const API_URL = 'http://localhost:5000/api/v0';
