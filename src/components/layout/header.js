e0ed8bf2c03b9e7ed1df549357790e180264cc38
Veljko Simic <veljko.simic98@gmail.com>
2016-05-12 15:00:46 +0200

    Move link in header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router';
 function Header() {
   return (
     <div>
-      <Link to="/logout/">Go to Logout</Link>
+      <Link to="/logout/">Go to Logout </Link>
+      <Link to="/clusters/">Clusters </Link>
+      <Link to="/services/">Services </Link>
+      <Link to="/tasks/">Tasks</Link>
     </div>
   );
 }
--- src/components/one-love.js
+++ src/components/one-love.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router';
 function OneLove() {
   return (
     <main className="app">
-      <Link to="/clusters/" > clusters </Link>
-      <Link to="/services/" > services</Link>
-      <Link to="/tasks/" > tasks</Link>
     </main>
   );
 }
