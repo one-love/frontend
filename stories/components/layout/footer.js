0918c380ead19139adfc25bc3de216d507aef5b0
Goran Mekić <meka@tilda.center>
2016-05-31 18:42:43 +0200

    Atomic design

--- stories/components/layout/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import Footer from '../../../src/components/layout/footer';
-import '../style.scss';
-
-storiesOf('Footer', module)
-  .add('footer', () => (
-    <Footer />
-  ));
