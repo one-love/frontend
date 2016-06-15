bca8cd232be14808f1970879d2a40293c807927e
Veljko Simic <veljko.simic98@gmail.com>
2016-06-15 22:26:54 +0200

    Create log molecule

--- stories/components/molecules/index.js
+++ stories/components/molecules/index.js
@@ -4,9 +4,11 @@ import { storiesOf } from '@kadira/storybook';
 import Header from './header';
 import Cluster from './cluster';
 import Service from './service';
+import Log from './provision_log';
 import '../style.scss';
 
 storiesOf('Molecules', module)
   .add('header', Header)
   .add('cluster', Cluster)
+  .add('log', Log)
   .add('service', Service);
