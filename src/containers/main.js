da53ca7dc5fe319238014134d83dcd57d306645e
Veljko Simic <veljko.simic98@gmail.com>
2016-06-27 22:31:52 +0200

    Create service-list

--- src/containers/main.js
+++ src/containers/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router';
 import { history } from '../constants';
 import { requireAuth } from '../utils';
+import Service from '../components/pages/service-list';
 import Login from '../components/pages/login';
 import Layout from '../components/layouts/layout';
 import '../sass/reset.scss';
@@ -13,6 +14,11 @@ const routes = {
     {
       path: '/',
       component: Layout,
+      onEnter: requireAuth,
+    },
+    {
+      path: '/services',
+      component: Service,
       onEnter: requireAuth,
     },
     {
--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import login from './components/pages/login/reducers';
 import sidebar from './components/atoms/sidebar/reducers';
+import serviceList from './components/pages/service-list/reducers';
 
 
 const reducers = {
   login,
   sidebar,
+  serviceList,
 };
 
 
