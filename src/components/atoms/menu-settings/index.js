35cd9b49cf4d02815ea20d47dfec51683b0c5075
Goran Mekić <meka@tilda.center>
2016-07-18 10:27:09 +0300

    Logout

--- src/components/atoms/menu-settings/index.js
+++ src/components/atoms/menu-settings/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import gear from './gear.svg';
 import cssModules from 'react-css-modules';
+import { history, postLogoutURL } from '../../../constants';
 import Sidebar from '../sidebar';
 import styles from './menu-settings.scss';
 
@@ -18,20 +19,30 @@ const MenuSettings = React.createClass({
     this.setState({ show: false });
   },
 
+  handleLogout() {
+    window.localStorage.removeItem('OneLoveAuthToken');
+    history.push(postLogoutURL);
+  },
+
   render() {
     const showSettings = this.state.show ? '' : this.showSettings;
     let content = '';
     if (this.state.show) {
       content = (
-        <div
-          className="disable"
-          onClick={this.hideSettings}
-          style={{
-            zIndex: 10,
-            marginTop: '-10px',
-          }}
-        >
-          x
+        <div>
+          <div
+            className="disable"
+            onClick={this.hideSettings}
+            style={{
+              zIndex: 10,
+              marginTop: '-10px',
+            }}
+          >
+            x
+          </div>
+          <div onClick={this.handleLogout}>
+            <u>Logout</u>
+          </div>
         </div>
       );
     }
