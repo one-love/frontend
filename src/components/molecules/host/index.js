83e3effd4766c3b3228c2bb39a9aeecb88140e1d
Goran Mekić <meka@tilda.center>
2016-07-19 00:26:35 +0300

    Add host to provider

--- src/components/molecules/host/index.js
+++ src/components/molecules/host/index.js
@@ -8,12 +8,21 @@ export default function Host(props) {
   if (props && props.name) {
     name = props.name;
   }
+  const iconId = `clusters/${props.iconId.clusterId}/providers/${props.iconId.providerName}/hosts/${props.iconId.hostname}`;
   return (
-    <Icon alt="host" img={svg} name={name} />
+    <Icon
+      alt="host"
+      img={svg}
+      name={name}
+      iconId={iconId}
+      close={props.close}
+    />
   );
 }
 
 
 Host.propTypes = {
   name: React.PropTypes.string,
+  iconId: React.PropTypes.object,
+  close: React.PropTypes.func,
 };
--- src/components/pages/provider/index.js
+++ src/components/pages/provider/index.js
@@ -3,16 +3,23 @@ import cssModules from 'react-css-modules';
 import { Link } from 'react-router';
 import styles from './provider.scss';
 import { connect } from 'react-redux';
-import actions from './actions/detail';
 import store from '../../../store';
 import List from '../../molecules/transition-appear';
 import Host from '../../molecules/host';
 import HostDetail from '../host';
+import Sidebar from '../../atoms/sidebar';
+import Add from '../../atoms/add';
+import createActions from '../host/actions/create';
+import hostActionsRemove from '../host/actions/remove';
+import actions from './actions/detail';
 
 
 const mapStateToProps = (state) => {
   const data = {
     provider: state.providerDetail.provider,
+    createStatus: state.hostCreate.status,
+    removeStatus: state.hostRemove.status,
+    remove: state.hostRemove.host,
   };
   return data;
 };
@@ -23,6 +30,23 @@ const ProviderDetail = React.createClass({
     children: React.PropTypes.node,
     provider: React.PropTypes.object,
     params: React.PropTypes.object,
+    remove: React.PropTypes.object,
+    removeStatus: React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      provider: {
+        hosts: [],
+      },
+    };
+  },
+
+  getInitialState() {
+    return {
+      hostname: '',
+      ip: '',
+    };
   },
 
   componentWillMount() {
@@ -33,13 +57,117 @@ const ProviderDetail = React.createClass({
       ));
   },
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.createStatus === 'success') {
+      store.dispatch(createActions.reset());
+      store.dispatch(actions.get(
+        this.props.params.clusterId,
+        this.props.params.providerName,
+      ));
+      this.setState({ create: false });
+    } else if (nextProps.removeStatus === 'success') {
+      store.dispatch(hostActionsRemove.reset());
+      store.dispatch(actions.get(
+        this.props.params.clusterId,
+        this.props.params.providerName,
+      ));
+      this.setState({ create: false });
+    }
+  },
+
   componentWillUnmount() {
     store.dispatch(actions.reset());
   },
 
+  showCreate() {
+    this.setState({ create: true });
+  },
+
+  hideCreate() {
+    this.setState({
+      create: false,
+      hostname: '',
+      ip: '',
+    });
+  },
+
+  handleHostnameChange(event) {
+    this.setState({ hostname: event.target.value });
+  },
+
+  handleIPChange(event) {
+    this.setState({ ip: event.target.value });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+    store.dispatch(createActions.create(
+      this.props.params.clusterId,
+      this.props.params.providerName,
+      this.state.hostname,
+      this.state.ip,
+    ));
+    this.setState({
+      hostname: '',
+      ip: '',
+      create: false,
+    });
+  },
+
+  handleRemove(event) {
+    event.preventDefault();
+    store.dispatch(hostActionsRemove.remove(
+      this.props.remove.id,
+    ));
+  },
+
+  handleCancel(event) {
+    event.preventDefault();
+    store.dispatch(hostActionsRemove.reset());
+  },
+
   render() {
-    if (!this.props.provider) {
-      return <div></div>;
+    if (this.props.removeStatus === 'confirm') {
+      return (
+        <div>
+          <h1>Remove host {this.props.remove.id}?</h1>
+          <button className="button" onClick={this.handleRemove}>yes</button>
+          <button className="button" onClick={this.handleCancel}>no</button>
+        </div>
+      );
+    }
+    let createHost = '';
+    if (this.state.create) {
+      createHost = (
+        <div>
+          <div className="disable" onClick={this.hideCreate}>x</div>
+          <h1>Create Host</h1>
+          <form role="form" onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="ip">IP</label>
+              <input
+                autoFocus
+                type="text"
+                id="ip"
+                placeholder="IP"
+                onChange={this.handleIPChange}
+                value={this.state.ip}
+              />
+            </div>
+            <div>
+              <label htmlFor="hostname">hostname</label>
+              <input
+                type="text"
+                id="hostname"
+                placeholder="hostname"
+                onChange={this.handleHostnameChange}
+                value={this.state.hostname}
+              />
+            </div>
+            <button className="button">Create</button>
+          </form>
+        </div>
+      );
     }
     const clusterId = this.props.params.clusterId;
     const providerName = this.props.params.providerName;
@@ -49,9 +177,18 @@ const ProviderDetail = React.createClass({
         {
           this.props.provider.hosts.map(host => {
             const link = `${providerUrl}/hosts/${host.hostname}`;
+            const identifier = {
+              clusterId: this.props.params.clusterId,
+              providerName: this.props.params.providerName,
+              hostname: host.hostname,
+            };
             return (
               <Link key={host.hostname} to={link}>
-                <Host name={host.hostname} />
+                <Host
+                  name={host.hostname}
+                  iconId={identifier}
+                  close={hostActionsRemove.confirm}
+                />
               </Link>
             );
           })
@@ -59,12 +196,18 @@ const ProviderDetail = React.createClass({
       </List>
     );
     return (
-      <div styleName="item">
+      <div>
+        <Sidebar show={this.state.create}>
+          {createHost}
+        </Sidebar>
         <div>
           <h2>Provider</h2>
           Name: {this.props.provider.name}
           <h3>Hosts</h3>
           {hosts}
+        </div>
+        <div onClick={this.showCreate}>
+          <Add />
         </div>
       </div>
     );
