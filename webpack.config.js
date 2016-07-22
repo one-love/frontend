35639dacaf2acbf32df3ee8e39d838c3b7288757
Goran Mekić <meka@tilda.center>
2016-07-22 22:50:35 +0200

    Unify dev and prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,10 @@
-/* eslint max-len: 0 */
-
-const path = require('path');
 const webpack = require('webpack');
-const precss = require('precss');
-const autoprefixer = require('autoprefixer');
+const config = require('./webpack.common');
 
-module.exports = {
-  devtool: 'cheap-module-source-map',
-  entry: ['./src/index'],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/',
-  },
-  plugins: [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'dev'" }),
-  ],
-  resolveLoader: {
-    fallback: path.join(__dirname, 'node_modules'),
-  },
-  module: {
-    preLoaders: [{
-      test: /\.js$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/,
-    }],
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['react-hot', 'babel'],
-        exclude: /node_modules/,
-        include: path.join(__dirname, 'src'),
-      },
-      {
-        test: /\.scss$/,
-        loaders: [
-          'style?sourceMap',
-          'css?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-          'postcss',
-          'sass?sourceMap',
-          'sass-resources',
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
-        ],
-      },
-    ],
-  },
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, './src/sass')],
-  },
-  sassResources: [
-    './src/sass/vars.scss',
-  ],
-  postcss() {
-    return [precss, autoprefixer];
-  },
-};
+
+config.plugins = [
+  new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'dev'" }),
+];
+
+
+module.exports = config;
