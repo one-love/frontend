a1e81c814b8677a3a366cd403cb387496a74c602
Veljko Simic <veljko.simic98@gmail.com>
2016-07-25 18:54:34 +0200

    Testing application page

--- src/test.js
+++ src/test.js
@@ -1,7 +1,9 @@
 /* eslint no-unused-expressions: 0 */
 import clusterPage from './components/pages/cluster/tests';
+import applicationPage from './components/pages/application/tests';
 
 
 /* PAGES */
 
 clusterPage;
+applicationPage;
