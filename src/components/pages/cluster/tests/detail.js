c2919924000f561c442aebc35a72ef39e78797d3
Veljko Simic <veljko.simic98@gmail.com>
2016-07-25 18:12:04 +0200

    Use real data for testing

--- src/components/pages/cluster/tests/detail.js
+++ src/components/pages/cluster/tests/detail.js
@@ -1,6 +1,7 @@
 /* eslint no-undef:  0 */
 /* eslint no-unused-expressions: 0 */
 /* eslint no-sequences: 0 */
+/* eslint quote-props: 0 */
 
 import expect from 'expect';
 import actions from '../actions/detail';
@@ -33,6 +34,123 @@ const clusterDetailTest = describe('Testing detail of cluster', () => {
           payload: {
             status: 'error',
             error: 'ErorR',
+          },
+          type: CLUSTER_DETAIL,
+        });
+  }),
+  it('get success state', () => {
+    expect(store.dispatch(actions.success({
+      'id': '579226dc168b3805106b00ea',
+      'name': 'Tammy',
+      'providers': [
+        {
+          'hosts': [
+            {
+              'hostname': 'target.vagrant',
+              'ip': '192.168.33.34',
+            },
+          ],
+          'name': 'Abigail',
+          'type': 'SSH',
+        },
+      ],
+      'roles': [
+        {
+          'admin': null,
+          'description': 'Administrator',
+          'name': 'admin',
+        },
+      ],
+      'services': [
+        {
+          'applications': [
+            {
+              'galaxy_role': 'onelove-roles.common',
+              'name': 'Joseph',
+            },
+          ],
+          'id': '579226dc168b3805106b00e9',
+          'name': 'Megan',
+          'user': {
+            'email': 'admin@example.com',
+            'first_name': null,
+            'id': '579226db168b3805106b00e7',
+            'last_name': null,
+            'roles': [
+              {
+                'admin': null,
+                'description': 'Administrator',
+                'name': 'admin',
+              },
+            ],
+            'username': 'admin',
+          },
+        },
+      ],
+      'username': 'vagrant',
+    })))
+        .toEqual({
+          payload: {
+            status: 'success',
+            cluster: {
+              'id': '579226dc168b3805106b00ea',
+              'name': 'Tammy',
+              'providers': [
+                {
+                  'hosts': [
+                    {
+                      'hostname': 'target.vagrant',
+                      'ip': '192.168.33.34',
+                    },
+                  ],
+                  'name': 'Abigail',
+                  'type': 'SSH',
+                },
+              ],
+              'roles': [
+                {
+                  'admin': null,
+                  'description': 'Administrator',
+                  'name': 'admin',
+                },
+              ],
+              'services': [
+                {
+                  'applications': [
+                    {
+                      'galaxy_role': 'onelove-roles.common',
+                      'name': 'Joseph',
+                    },
+                  ],
+                  'id': '579226dc168b3805106b00e9',
+                  'name': 'Megan',
+                  'user': {
+                    'email': 'admin@example.com',
+                    'first_name': null,
+                    'id': '579226db168b3805106b00e7',
+                    'last_name': null,
+                    'roles': [
+                      {
+                        'admin': null,
+                        'description': 'Administrator',
+                        'name': 'admin',
+                      },
+                    ],
+                    'username': 'admin',
+                  },
+                },
+              ],
+              'username': 'vagrant',
+            },
+
+            roles: [
+              {
+                'admin': null,
+                'description': 'Administrator',
+                'name': 'admin',
+              },
+            ],
+
           },
           type: CLUSTER_DETAIL,
         });
--- src/components/pages/cluster/tests/edit.js
+++ src/components/pages/cluster/tests/edit.js
@@ -1,6 +1,8 @@
 /* eslint no-undef: 0 */
 /* eslint no-unused-expressions: 0 */
 /* eslint no-sequences: 0 */
+/* eslint quote-props: 0 */
+
 import expect from 'expect';
 import actions from '../actions/edit';
 import store from '../../../../store';
@@ -18,10 +20,108 @@ const clusterEditTest = describe('Testing edit of cluster', () => {
       });
   }),
   it('get success state', () => {
-    expect(store.dispatch(actions.success({})))
+    expect(store.dispatch(actions.success({
+      'id': '579226dc168b3805106b00ea',
+      'name': 'Tammy',
+      'providers': [
+        {
+          'hosts': [
+            {
+              'hostname': 'target.vagrant',
+              'ip': '192.168.33.34',
+            },
+          ],
+          'name': 'Abigail',
+          'type': 'SSH',
+        },
+      ],
+      'roles': [
+        {
+          'admin': null,
+          'description': 'Administrator',
+          'name': 'admin',
+        },
+      ],
+      'services': [
+        {
+          'applications': [
+            {
+              'galaxy_role': 'onelove-roles.common',
+              'name': 'Joseph',
+            },
+          ],
+          'id': '579226dc168b3805106b00e9',
+          'name': 'Megan',
+          'user': {
+            'email': 'admin@example.com',
+            'first_name': null,
+            'id': '579226db168b3805106b00e7',
+            'last_name': null,
+            'roles': [
+              {
+                'admin': null,
+                'description': 'Administrator',
+                'name': 'admin',
+              },
+            ],
+            'username': 'admin',
+          },
+        },
+      ],
+      'username': 'vagrant',
+    })))
       .toEqual({
         payload: {
-          cluster: {},
+          cluster: {
+            'id': '579226dc168b3805106b00ea',
+            'name': 'Tammy',
+            'providers': [
+              {
+                'hosts': [
+                  {
+                    'hostname': 'target.vagrant',
+                    'ip': '192.168.33.34',
+                  },
+                ],
+                'name': 'Abigail',
+                'type': 'SSH',
+              },
+            ],
+            'roles': [
+              {
+                'admin': null,
+                'description': 'Administrator',
+                'name': 'admin',
+              },
+            ],
+            'services': [
+              {
+                'applications': [
+                  {
+                    'galaxy_role': 'onelove-roles.common',
+                    'name': 'Joseph',
+                  },
+                ],
+                'id': '579226dc168b3805106b00e9',
+                'name': 'Megan',
+                'user': {
+                  'email': 'admin@example.com',
+                  'first_name': null,
+                  'id': '579226db168b3805106b00e7',
+                  'last_name': null,
+                  'roles': [
+                    {
+                      'admin': null,
+                      'description': 'Administrator',
+                      'name': 'admin',
+                    },
+                  ],
+                  'username': 'admin',
+                },
+              },
+            ],
+            'username': 'vagrant',
+          },
           status: 'success',
         },
         type: CLUSTER_EDIT,
