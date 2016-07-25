75760bc3eb40331e6d9ba78b7713fe6224f5d5e3
Veljko Simic <veljko.simic98@gmail.com>
2016-07-25 17:36:31 +0200

    Testing cluster page

---
+++ src/components/pages/cluster/tests/edit.js
@@ -0,0 +1,51 @@
+/* eslint no-undef: 0 */
+/* eslint no-unused-expressions: 0 */
+/* eslint no-sequences: 0 */
+import expect from 'expect';
+import actions from '../actions/edit';
+import store from '../../../../store';
+import { CLUSTER_EDIT } from '../constants';
+
+
+const clusterEditTest = describe('Testing edit of cluster', () => {
+  it('get initial state', () => {
+    expect(store.dispatch(actions.reset()))
+      .toEqual({
+        payload: {
+          status: 'initial',
+        },
+        type: CLUSTER_EDIT,
+      });
+  }),
+  it('get success state', () => {
+    expect(store.dispatch(actions.success({})))
+      .toEqual({
+        payload: {
+          cluster: {},
+          status: 'success',
+        },
+        type: CLUSTER_EDIT,
+      });
+  }),
+  it('get pending state', () => {
+    expect(store.dispatch(actions.begin()))
+      .toEqual({
+        payload: {
+          status: 'pending',
+        },
+        type: CLUSTER_EDIT,
+      });
+  }),
+  it('get error state', () => {
+    expect(store.dispatch(actions.fail('ErorR')))
+      .toEqual({
+        payload: {
+          status: 'error',
+          error: 'ErorR',
+        },
+        type: CLUSTER_EDIT,
+      });
+  });
+});
+
+export default clusterEditTest;
