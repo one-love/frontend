368597b9cd31e1d00c96fc8b2863653a790a1a9c
TornjV <tornjanski.veljko@gmail.com>
2016-08-21 16:16:26 +0200

    Local fonts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/root';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
+import './fonts/fonts.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
