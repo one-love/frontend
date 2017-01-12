e1409afb986bf16242f5e13bc87bb69cf68fcde4
wex <wex@tilda.center>
2017-01-12 12:22:27 +0100

    Use es6 syntax for functions

--- src/test/molecules/service.test.js
+++ src/test/molecules/service.test.js
@@ -2,8 +2,6 @@
 /* eslint no-unused-expressions: 0 */
 /* eslint no-sequences: 0 */
 /* eslint quotes: 0 */
-/* eslint func-names: 0 */
-/* eslint prefer-arrow-callback: 0 */
 import React from 'react';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
@@ -14,7 +12,7 @@ import ServiceIcon from 'material-ui/svg-icons/action/build';
 import styles from '../../components/atoms/icon/styles';
 
 
-describe("Molecule Service", function () {
+describe("Molecule Service", () => {
   const renderer = ReactTestUtils.createRenderer();
   renderer.render(
     <Service
@@ -24,26 +22,26 @@ describe("Molecule Service", function () {
   );
   const result = renderer.getRenderOutput();
 
-  it("Service should be return paper element", function () {
+  it("Service should be return paper element", () => {
     expect(result.type).to.equal(Paper);
     expect(result.props.style).to.equal(styles.paper);
   });
 
-  it("First child", function () {
+  it("First child", () => {
     const child = result.props.children[0];
     expect(child.type).to.equal('div');
     expect(child.props.children).to.equal('x');
     expect(child.props.style).to.equal(styles.close);
   });
 
-  it("Second child", function () {
+  it("Second child", () => {
     const child = result.props.children[1];
     expect(child.type).to.equal(ServiceIcon);
     expect(child.props.style).to.equal(styles.icon);
     expect(child.props.color).to.equal(styles.icon.color);
   });
 
-  it("Third child", function () {
+  it("Third child", () => {
     const child = result.props.children[2];
     expect(child.type).to.equal('div');
     expect(child.props.children).to.equal('vagrant');
