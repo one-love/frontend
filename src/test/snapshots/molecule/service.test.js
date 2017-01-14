e915b33926d2526a40dcb93138dd3ccd740f77dd
wex <wex@tilda.center>
2017-01-14 18:46:05 +0100

    Snapshot for service create form

--- src/test/snapshots/molecule/service.test.js
+++ src/test/snapshots/molecule/service.test.js
@@ -9,16 +9,16 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 describe("Service Molecule (snapshot)", () => {
-  it("Service molecule render paper", () => {
-    const setup = () => {
-      const props = {
-        name: "vagrant",
-        serviceId: "1337testId",
-        confirm: jest.fn(),
-      };
-      return shallow(<Service {...props} />);
+  const setup = () => {
+    const props = {
+      name: "vagrant",
+      serviceId: "1337testId",
+      confirm: jest.fn(),
     };
+    return shallow(<Service {...props} />);
+  };
 
+  it("Service molecule render paper", () => {
     const component = setup();
     expect(toJson(component)).toMatchSnapshot();
   });
