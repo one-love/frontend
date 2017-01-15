53d652e0feb94efb1f16c7bf6115bb2dd65acd40
wex <wex@tilda.center>
2017-01-15 16:41:59 +0100

    Simulate submit

--- src/test/molecules/serviceCreateForm.test.js
+++ src/test/molecules/serviceCreateForm.test.js
@@ -5,10 +5,13 @@
 import React from 'react';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 
 import CreateServiceForm from '../../components/molecules/service/createForm';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 describe(" Create Service form", () => {
   const setup = () => {
@@ -19,9 +22,26 @@ describe(" Create Service form", () => {
     return renderer.getRenderOutput();
   };
 
+  const mountWithContext = (node) => {
+    const muiTheme = getMuiTheme();
+    return mount(node, {
+      context: { muiTheme },
+      childContextTypes: { muiTheme: React.PropTypes.object },
+    });
+  };
+
   const result = setup();
-  it("Render one TextField", () => {
+  it("Render one form", () => {
     expect(result.type).to.equal('form');
+  });
+
+  it("Form handle submit", () => {
+    const handleSubmit = sinon.spy();
+    const wrapper = mountWithContext(<CreateServiceForm />);
+    wrapper.node.handleSubmit = handleSubmit;
+    wrapper.setState({ name: 'TotalBrutal' });
+    wrapper.simulate('submit');
+    expect(handleSubmit.calledOnce).to.equal(true);
   });
   it("Test children", () => {
     const firstChild = result.props.children[0];
@@ -43,5 +63,7 @@ describe(" Create Service form", () => {
     const RaisedButtonChild = secondChild.props.children;
     expect(RaisedButtonChild.type).to.equal(RaisedButton);
     expect(RaisedButtonChild.props.label).to.equal('Create');
+  });
+  it("RaisedButton is clickable", () => {
   });
 });
