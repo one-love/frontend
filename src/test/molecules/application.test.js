e566fde39f6b63cb3e262b29f6b707df87db4285
wex <wex@tilda.center>
2017-01-16 12:13:25 +0100

    Create application test

--- src/test/molecules/application.test.js
+++ src/test/molecules/application.test.js
@@ -6,24 +6,23 @@ import React from 'react';
 import { expect } from 'chai';
 import ReactTestUtils from 'react-addons-test-utils';
 
-import { Host } from '../../components/molecules/host';
+import { Application } from '../../components/molecules/application';
 import Paper from 'material-ui/Paper';
-import HostIcon from 'material-ui/svg-icons/hardware/memory';
+import ApplicationIcon from 'material-ui/svg-icons/action/dashboard';
 import styles from '../../components/atoms/icon/styles';
 
 
-describe("Molecule Host", () => {
+describe("Molecule Applcation", () => {
   const setup = () => {
     const renderer = ReactTestUtils.createRenderer();
     const props = {
-      name: "DO",
-      clusterId: "1231231212123",
-      providerName: "DigitalOcean",
+      name: "ngnix",
+      serviceId: "808080",
       confirm: jest.fn(),
     };
 
     renderer.render(
-      <Host {...props} />
+      <Application {...props} />
     );
 
     return renderer.getRenderOutput();
@@ -31,24 +30,24 @@ describe("Molecule Host", () => {
 
   const result = setup();
 
-  it("Host return paper element", () => {
+  it("Application return paper element", () => {
     expect(result.type).to.equal(Paper);
     expect(result.props.style).to.equal(styles.paper);
   });
 
-  it("Testing children of host", () => {
+  it("Testing children of app", () => {
     const firstChild = result.props.children[0];
     expect(firstChild.type).to.equal('div');
     expect(firstChild.props.children).to.equal('x');
     expect(firstChild.props.style).to.equal(styles.close);
 
     const secondChild = result.props.children[1];
-    expect(secondChild.type).to.equal(HostIcon);
+    expect(secondChild.type).to.equal(ApplicationIcon);
     expect(secondChild.props.style).to.equal(styles.icon);
     expect(secondChild.props.color).to.equal(styles.icon.color);
 
     const thirdChild = result.props.children[2];
     expect(thirdChild.type).to.equal('div');
-    expect(thirdChild.props.children).to.equal('DO');
+    expect(thirdChild.props.children).to.equal('ngnix');
   });
 });
