d11e62e59e5777f7500810b1832b4e66f9960e29
Goran Mekić <meka@tilda.center>
2017-01-25 01:58:25 +0100

    Test the environment

--- bin/dev.sh
+++ bin/dev.sh
@@ -5,8 +5,10 @@ export NODE_ENV="dev"
 cd ${PROJECT_ROOT}
 
 if [ -z $(which git) ]; then
-    apt-get update
-    apt-get install -y git-core
+    if [ $(uname) = "Linux" ]; then
+        apt-get update
+        apt-get install -y git-core
+    fi
 fi
 npm install
 npm start
