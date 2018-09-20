cc63765dce8491a6135a5cd332facfdf9eed11ca
Goran Mekić <meka@tilda.center>
2018-09-21 01:13:37 +0200

    Run tests

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,9 @@
 
 BIN_DIR=`dirname $0`
 . "${BIN_DIR}/common.sh"
+if [ ! -e "${PROJECT_ROOT}/project.conf" ];  then
+  echo 'HTTP_PROXY=http://localhost:5000' >"${PROJECT_ROOT}/project.conf"
+fi
 setup
 
 "${PACKAGE_MANAGER}" test --coverage --color
